e553f995d4b4eac6aec46fc307b71b7e65744558
Horneteater <MohammadAliSefidaniforough@outlook.com>
2026-09-11 15:45:51 +0330

    Add files via upload

---
+++ Googology/Skewe.py
@@ -0,0 +1,42 @@
+
+
+
+"""
+a program to calculate the number of numbers of Skewe's number
+
+"""
+from math import log10 , floor
+
+S=10**(10**(10**34))
+
+def digits(n):
+    """
+    returns the number of digits of a number
+    
+    """
+    return floor(log10(n)) + 1
+    
+
+print(digits(S))
+
+
+"""
+
+Ok let's be real here there is no computer that can finish this program in the universe.
+
+so let me just do it for you with maths.
+
+since log10(10**x)==x 
+
+the number of numbers of Skewe's number is 10**(10**34) given that the universe is 10**186 planck volume (the smallest meaningful unit of volume) you couldn't write this number in the observable universe or multiple universes
+
+the number of numbers of numbers of Skewe's number is 10**34
+
+
+the number of numbers of numbers of numbers of Skewe's number is 34
+
+
+
+there you have it folks, have a good one
+
+"""
